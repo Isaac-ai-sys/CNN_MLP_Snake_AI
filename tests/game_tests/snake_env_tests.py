1b73a013dc8bd2f46369893f30e785c4e905a946
hillbillyjoe42 <isaacwithrow2024@gmail.com>
2026-04-25 14:01:09 -0700

    added small testing functionality

---
+++ tests/game_tests/snake_env_tests.py
@@ -0,0 +1,10 @@
+import pytest
+from game.snake_env import Snake_Env
+
+@pytest.fixture
+def env():
+    return Snake_Env()
+
+def test_env_reset(env):
+    state = env.reset()
+    assert state is not None
